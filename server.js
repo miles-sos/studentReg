c4d5d894e6e30b165930afa4bf7f568f69070367
Miles <frank.ebuzoeme@gmail.com>
2021-10-11 10:07:17 +0100

    updated ATLAS_DB Configuration

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 
+/* Laod Config */
+dotenv.config({ path: './config/config.env' });
+
 /* Require DBConnection */
 const { connectDB } = require('./config/db');
+
+/* Invoking the DB_connect function */
+// connectDB();
+
+/* Connecting to MongoDB ATLAS */
+require('mongoose')
+  .connect(process.env.ATLAS_DB)
+  .then((con) => console.log(`Connect to ATLAS DB: ${con.connection.host}`))
+  .catch((err) => console.log(`Could not connect: ${err}`));
 
 /* Middlewares */
 app.use(express.json());
@@ -11,16 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-/* Laod Config */
-dotenv.config({ path: './config/config.env' });
-connectDB();
-
 /* FIRST PATTERN */
 app.use('/api/students', require('./routes/index'));
 
 /* SECOND PATTERN */
 // const loader = require('./routes/index');
 // loader(app);
+/////////////// OR //////////////////
+// require('./routes/index')(app);
 
 /* Routes */
 app.get('/', (req, res) => {
@@ -30,8 +40,6 @@ app.get('/', (req, res) => {
 app.post('/api/students', (req, res) => {
   res.redirect('');
 });
-
-// require('./routes/index')(app);
 
 /* Set port, Listen for requests */
 const PORT = process.env.PORT || 8080;
